257fd839fc1b9a298948961089ef736eacec759d
Vadim Ushakov <vadimushakov33@gmail.com>
2024-04-08 21:54:41 +0300

    adding test_strlen_

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <assert.h>
 #include "string/string_.h"
 
+// 1
+
 void test_length_of_string_is_not_empty() {
     char s[] = "Hello";
 
@@ -26,8 +28,38 @@ void test_length_of_string() {
     test_length_of_string_is_exceeds_the_size();
 }
 
+// 2
+
+void test_strlen_is_not_empty() {
+    char s[] = "\tHello\t";
+
+    assert(strlen_(s) == 7);
+}
+
+void test_strlen_is_empty() {
+    char s[] = "";
+
+    assert(strlen_(s) == 0);
+}
+
+void test_strlen_() {
+    test_strlen_is_not_empty();
+    test_strlen_is_empty();
+}
+
+
+
+
+
+
+
+
+
+
+
 void test () {
     test_length_of_string();
+    test_strlen_();
 }
 
 int main() {
--- string/string_.h
+++ string/string_.h
@@ -3,7 +3,10 @@
 #ifndef LAB_WORK_17_STRING__H
 #define LAB_WORK_17_STRING__H
 
-//
+// возвращает длину строки
 int length_of_string(char *str);
+
+//
+size_t strlen_(const char *begin);
 
 #endif //LAB_WORK_17_STRING__H
