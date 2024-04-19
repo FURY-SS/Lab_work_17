c48333e848c81faa722f4435a8f6a9baabfc51bc
Vadim Ushakov <vadimushakov33@gmail.com>
2024-04-19 12:18:47 +0300

    adding tests for replace_word_to_word

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include "string/tasks/remove_adjacent_equal_letters.h"
 #include "string/tasks/reverse_str.h"
 #include "string/tasks/replace_digit_to_space.h"
+#include "string/tasks/replace_word_to_word.h"
 
 int main() {
     test_1_remove_non_letters();
     test_2_remove_adjacent_equal_letters();
     test_3_reverse_word();
     test_4_replace_digit_to_space();
+    test_5_replace_word_to_word();
 }
