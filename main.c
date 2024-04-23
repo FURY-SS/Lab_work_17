ab3d7ceea6cdf833df50b006ac6c33c8a909d465
Vadim Ushakov <vadimushakov33@gmail.com>
2024-04-23 10:34:10 +0300

    adding tests for find_preceding_word

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 #include "string/tasks/find_duplicate_words.h"
 #include "string/tasks/find_with_the_same_letters_words.h"
 #include "string/tasks/get_word_except_last.h"
+#include "string/tasks/find_preceding_word.h"
 
 int main() {
     test_1_remove_non_letters();
@@ -30,4 +31,5 @@ int main() {
     test_13_find_duplicate_words();
     test_14_find_with_the_same_letters_words();
     test_15_get_word_except_last();
+    test_16_find_preceding_word();
 }
